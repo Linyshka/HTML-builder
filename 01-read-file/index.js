2925e1084788a4dbeddae4a48e78499b56a1e94e
Алина Радюкевич <alina.makaroshka@outlook.com>
2023-05-04 01:20:05 +0300

    feat: add the implementation of the 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+const currentPath = path.join(__dirname, "text.txt");
+const readableStream = fs.createReadStream(currentPath, "utf-8");
+let data = "";
+
+readableStream.on('data', chunk => data += chunk);
+readableStream.on("end", () => console.log(data));
